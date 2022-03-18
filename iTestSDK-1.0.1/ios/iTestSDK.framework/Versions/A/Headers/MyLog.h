5197097869f7b9e1526b3e37171718e71cdf8bbd
helenhong <helenhong@tencent.com>
2022-03-18 16:56:14 +0800

    1.0.2

---
+++ iTestSDK-1.0.1/ios/iTestSDK.framework/Versions/A/Headers/MyLog.h
@@ -0,0 +1,17 @@
+//
+//  MyLog.h
+//  AFNetworking
+//
+//  Created by 洪一敏 on 2022/3/18.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyLog : NSObject
+- (void)print;
+@end
+
+NS_ASSUME_NONNULL_END
